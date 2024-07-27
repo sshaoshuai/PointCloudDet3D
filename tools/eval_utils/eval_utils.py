76b4c8ecb12adaa5e037bbd1f2ff98b43f93c883
kmodexc <kmode@kmode.dev>
2024-07-27 21:26:25 +0000

    added support for dece

--- tools/eval_utils/eval_utils.py
+++ tools/eval_utils/eval_utils.py
@@ -7,6 +7,7 @@ import tqdm
 
 from pcdet.models import load_data_to_gpu
 from pcdet.utils import common_utils
+from pcdet.datasets import DataLoader
 
 
 def statistics_info(cfg, ret_dict, metric, disp_dict):
@@ -19,7 +20,7 @@ def statistics_info(cfg, ret_dict, metric, disp_dict):
         '(%d, %d) / %d' % (metric['recall_roi_%s' % str(min_thresh)], metric['recall_rcnn_%s' % str(min_thresh)], metric['gt_num'])
 
 
-def eval_one_epoch(cfg, args, model, dataloader, epoch_id, logger, dist_test=False, result_dir=None):
+def eval_one_epoch(cfg, args, model, dataloader : DataLoader, epoch_id, logger, dist_test=False, result_dir=None):
     result_dir.mkdir(parents=True, exist_ok=True)
 
     final_output_dir = result_dir / 'final_result' / 'data'
