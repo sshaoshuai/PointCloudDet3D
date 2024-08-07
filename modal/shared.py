e3b448baebbb7132a3bcf5859fa1dcb15e43b9ec
Oscar Beijbom <oscar.beijbom@gmail.com>
2024-08-06 23:13:49 -0700

    trying a to build spconv and cumm from source; adding debug messages

--- modal/shared.py
+++ modal/shared.py
@@ -18,7 +18,6 @@ pcdet_cuda_image = (
     .run_commands("cp -r /root/OpenPCDet/pcdet /root/pcdet") # Copy the pcdet folder to the root
     .run_commands("cp /root/OpenPCDet/setup.py /root/setup.py") # Copy the setup.py file to the root
     .run_commands("export TORCH_CUDA_ARCH_LIST='6.0 6.1 7.0 7.5 8.0' && cd /root/ && python setup.py develop")  # Install OpenPCDet
-    .pip_install(["spconv-cu120==2.3.6"])
     .copy_local_dir("../tools", "/root/tools") # Copy the tools folder to the container. This means you can change configs files and run them without having to commit the changes.
     .copy_local_dir("../pcdet", "/root/pcdet") # For debugging purposes
 )
